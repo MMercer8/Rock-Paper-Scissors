548d569609c64e425c0bccd3cf9055cbe41749d3
Michael Mercer <michaelmercer89@gmail.com>
2022-08-17 00:31:52 -0400

    Game logic update. Add tracking for wins/losses/ties. Add prompt for choice per each round. Additional misc. updates

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let playerChoice = "Rock";
+let playerWins = 0;
+let playerTies = 0;
+let playerLosses = 0;
 function getComputerChoice() {
     let choices = ((Math.floor(Math.random()*3))+1);
     if(choices === 1) {
@@ -15,7 +18,7 @@ function getComputerChoice() {
 }
 getComputerChoice();
 
-function chooseWinner(playerChoice, computerChoice) {
+function playRound(playerChoice, computerChoice) {
 
     let choice = playerChoice.toLowerCase();
     console.log("Player choice is: " + choice);
@@ -23,34 +26,43 @@ function chooseWinner(playerChoice, computerChoice) {
     if (choice === "rock") {
         if(computerChoice === "Rock") {
             //console.log("TIE!! Rock vs Rock");
+            playerTies++;
             return "TIE!! Rock vs Rock";
         }else if (computerChoice === "Paper") {
             //console.log("You lose! Rock vs Paper");
+            playerLosses++;
             return "You lose! Rock vs Paper";
         } else {
             //console.log("You Win! Rock vs Scissors");
+            playerWins++;
             return "You Win! Rock vs Scissors";
         }
     } else if (playerChoice === "paper") {
         if(computerChoice === "Rock") {
             //console.log("You Win! Paper vs Rock");
+            playerWins++;
             return "You Win! Paper vs Rock";
         }else if (computerChoice === "Paper") {
             //console.log("TIE!! Paper vs Paper");
+            playerTies++;
             return "TIE!! Paper vs Paper";
         } else {
-            //console.log("TIE!! Paper vs Scissors");
-            return "TIE!! Paper vs Scissors";
+            //console.log("You Lose!! Paper vs Scissors");
+            playerLosses++;
+            return "You Lose!! Paper vs Scissors";
         }
     } else if (playerChoice === "scissors") {
         if(computerChoice === "Rock") {
             //console.log("You Lose!! Scissors vs Rock");
+            playerLosses++;
             return "You Lose!! Scissors vs Rock";
         }else if (computerChoice === "Paper") {
             //console.log("You Win!! Scissors vs Paper");
+            playerWins++;
             return "You Win!! Scissors vs Paper";
         } else {
            // console.log("TIE!! Scissors vs Scissors");
+            playerTies++;
             return "TIE!! Scissors vs Scissors";
         }
     } else {
@@ -60,10 +72,47 @@ function chooseWinner(playerChoice, computerChoice) {
 
 }
 
+function game() {
+    playerWins = 0;
+    playerTies = 0;
+    playerLosses = 0;
+    
+    for (let i = 0; i < 5; i++) {
+        playerChoice = prompt("Choose your weapon: Rock, Paper, Scissors.");
+        console.log(playRound(playerChoice, getComputerChoice()));
+        
+    }
+    console.log("Wins: " + playerWins + " Player Losses: " + playerLosses + " Ties: " + playerTies);
+    if (playerWins >= 3) {
+        //player wins!
+        console.log("Player Wins!");
+    } else if (playerLosses >= 3) {
+        //player loses
+        console.log("Player loses!");
+    } else if (playerWins == 2 && playerLosses <= 1) {
+        //player wins
+        console.log("Player Wins!");
+    } else if (playerWins == 1 && playerLosses == 0) {
+        //player wins
+        console.log("Player Wins!");
+    } else if (playerLosses == 2 && playerWins <= 1) {
+        //player loses
+        console.log("Player loses!");
+    } else if (playerLosses == 1 && playerWins == 0) {
+        //player loses 
+        console.log("Player loses!");
+    } else {
+        //player tied
+        console.log("Players Tied!");
+    }
+}
+
+game();
 
 
 
-console.log(chooseWinner(playerChoice, getComputerChoice()));
+
+//console.log(playRound(playerChoice, getComputerChoice()));
 
 //hat will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
 //when called generate random number
@@ -83,7 +132,11 @@ console.log(chooseWinner(playerChoice, getComputerChoice()));
 8) Scissor Paper
 9) Scissor Scissor
 
-
+Game()
+call play round function
+play 5 rounds.
+keep track of wins
+at end of 5 rounds show winner or loser
 
 */
 
@@ -92,3 +145,4 @@ console.log(chooseWinner(playerChoice, getComputerChoice()));
 //make computer choice
 //compare two and choose winner
 //display winner or loser
+
